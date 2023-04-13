f10839ba1585f6dc9ce9b6f23eccd028becf0960
sanverakif <sanveremreakif@gmail.com>
2023-04-14 00:31:08 +0300

    custom directive-switch-style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { NgForComponent } from './directives/ng-for/ng-for.component';
 import { NgIfComponent } from './directives/ng-if/ng-if.component';
 import { NgSwitchComponent } from './directives/ng-switch/ng-switch.component';
 import { NgClassComponent } from './directives/ng-class/ng-class.component';
+import { NgStyleComponent } from './directives/ng-style/ng-style.component';
+import { NgModelComponent } from './directives/ng-model/ng-model.component';
+import { CustomDirective } from './directives/custom.directive';
 
 @NgModule({
   declarations: [
@@ -30,6 +33,9 @@ import { NgClassComponent } from './directives/ng-class/ng-class.component';
     NgIfComponent,
     NgSwitchComponent,
     NgClassComponent,
+    NgStyleComponent,
+    NgModelComponent,
+    CustomDirective,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
