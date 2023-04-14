23641c0dce4fa2c3de03f82c53be4114a50b9ab3
sanverakif <sanveremreakif@gmail.com>
2023-04-15 01:02:44 +0300

    pipe work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { NgClassComponent } from './directives/ng-class/ng-class.component';
 import { NgStyleComponent } from './directives/ng-style/ng-style.component';
 import { NgModelComponent } from './directives/ng-model/ng-model.component';
 import { CustomDirective } from './directives/custom.directive';
+import { PipeComponent } from './pipes/pipe/pipe.component';
+import { UpperPipe } from './pipes/upper.pipe';
 
 @NgModule({
   declarations: [
@@ -36,6 +38,8 @@ import { CustomDirective } from './directives/custom.directive';
     NgStyleComponent,
     NgModelComponent,
     CustomDirective,
+    PipeComponent,
+    UpperPipe,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
